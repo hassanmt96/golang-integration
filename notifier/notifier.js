7bac23b294df6ba752a1d02a27ad852f1fb73193
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2021-04-02 21:24:07 -0500

    testing to see if node with express is working

---
+++ notifier/notifier.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => console.log(`server is up and running on port: ${port}`))
