6ae76f8334fac5e7d7a0b786943b651e6991447d
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2021-04-02 21:52:51 -0500

    debugging error from post route

--- notifier/notifier.js
+++ notifier/notifier.js
@@ -1,23 +1,18 @@
+const path = require('path');
+const notifier = require('node-notifier');
 const express = require('express');
-const bodyParser = require('body-parser');
-const { notify } = require('node-notifier');
 const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 9000;
 
-
-app.use(bodyParser.json())
-
-
-app.get("/health", (req, res) => res.status(200).send())
-
-app.post("/notify", (req, res) => {
+app.use(bodyParser.json());
+app.get('/health', (req, res) => res.status(200).send());
+app.post('/notify', (req, res) => {
     notify(req.body, reply => res.send(reply))
-})
+});
 
-
-app.listen(port, () => console.log(`server is up and running on port: ${port}`))
+app.listen(port, () => console.log(`server is up and running on port: ${port}`));
 
 const notify = ({title, message}, cb) => {
-    cb("some string")
-
-}
+  cb('some string')
+};
