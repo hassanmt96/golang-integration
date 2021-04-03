3b2dcabd482fe02251d14345e00da2e4fe14ad10
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2021-04-02 21:38:24 -0500

    testing post endpoint for notifications

--- notifier/notifier.js
+++ notifier/notifier.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { notify } = require('node-notifier');
 const app = express();
 const port = process.env.PORT || 9000;
 
 
+app.use(bodyParser.json())
+
+
 app.get("/health", (req, res) => res.status(200).send())
 
+app.post("/notify", (req, res) => {
+    notify(req.body, reply => res.send(reply))
+})
+
+
 app.listen(port, () => console.log(`server is up and running on port: ${port}`))
+
+const notify = ({title, message}, cb) => {
+    cb("some string")
+
+}
